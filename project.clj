51ba4fbf686908bd9863bf881b6204a267ee6bda
Donald Clark Jackson <dcj@clark-communications.com>
2012-06-11 20:21:52 -0700

    Clojure 1.4, upgrade to clojure.data.jdbc 0.2.2

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject korma "0.3.0-beta10"
+(defproject net.likestream/korma "0.3.0-beta10"
   :description "Tasty SQL for Clojure"
   :url "http://github.com/ibdknox/korma"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [c3p0/c3p0 "0.9.1.2"]
-                 [org.clojure/java.jdbc "0.1.3"]]
+                 [org.clojure/java.jdbc "0.2.2"]]
   :codox {:exclude [korma.sql.engine korma.sql.fns korma.sql.utils]}
-          :dev-dependencies [[postgresql "9.0-801.jdbc4"]])
+  )
+
+
